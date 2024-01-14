5b8aacee72b79770907f371a08e77152bbf69896
pedzero <pedro9669silva@gmail.com>
2024-01-14 20:18:28 -0300

    feat: Schedule asynchronous store update checks using node-cron

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import { checkStoreUpdates } from './api/checkStoreUpdates.js';
+import cron from 'node-cron';
 
 const twitchChannelId = '5cc799026e852d26fcf16717';
 
-console.log(await checkStoreUpdates(twitchChannelId));
+cron.schedule('*/15 * * * * *', async () => {
+    try {
+        console.log('Running store update check...');
+        console.log(await checkStoreUpdates(twitchChannelId));
+    } catch (error) {
+        console.error('Error during store update check:', error.message);
+    }
+});
