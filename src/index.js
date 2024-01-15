9f810b8b5c57a9de9e9efc6b08ef0e59c03915c0
pedzero <pedro9669silva@gmail.com>
2024-01-15 20:47:34 -0300

    refactor: Use of new features

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { checkStoreUpdates } from './api/checkStoreUpdates.js';
+import { getChannelDetails } from './api/getChannelDetails.js';
+import { buildEmail } from './email/buildEmail.js';
 import { sendEmail } from './email/notify.js';
 import cron from 'node-cron';
 import fs from 'fs/promises';
 
 let config;
+
 try {
     const configFile = await fs.readFile('./config.json', 'utf-8');
     config = JSON.parse(configFile);
@@ -17,15 +20,15 @@ cron.schedule(config.updateIntervalMinutes, async () => {
         console.log('Running store update check...');
         for (const channel of config.watchedChannels) {
             const newItems = await checkStoreUpdates(channel);
+            const channelDetails = await getChannelDetails(channel);
 
-            console.log(newItems);
-
-            /*if (updatedItems.length > 0) {
+            if (newItems.length > 0) {
+                const emailMessage = buildEmail(channelDetails, newItems);
                 for (const recipient of config.emailRecipients) {
                     console.log("notifying: ", recipient);
-                    await sendEmail(`New items in Twitch store for channel ${channel}!`, `New items: ${JSON.stringify(updatedItems)}`, recipient);
+                    await sendEmail(`New items in the ${channelDetails.displayName} Twitch store!`, emailMessage, recipient);
                 }
-            }*/
+            }
         }
     } catch (error) {
         console.error('Error during store update check:', error.message);
