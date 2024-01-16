899451a107a3b22b694e698239cf1bd7ecb52801
pedzero <pedro9669silva@gmail.com>
2024-01-15 21:10:24 -0300

    refactor: Add new log messages

--- src/index.js
+++ src/index.js
@@ -17,18 +17,28 @@ try {
 
 cron.schedule(config.updateIntervalMinutes, async () => {
     try {
+        let newItensFound = false;
         console.log('Running store update check...');
         for (const channel of config.watchedChannels) {
             const newItems = await checkStoreUpdates(channel);
             const channelDetails = await getChannelDetails(channel);
 
             if (newItems.length > 0) {
+                newItensFound = true;
+
+                const channelName = channelDetails.displayName;
                 const emailMessage = buildEmail(channelDetails, newItems);
+
+                console.log(`New items found in ${channelName} channel.`);
                 for (const recipient of config.emailRecipients) {
-                    console.log("notifying: ", recipient);
-                    await sendEmail(`New items in the ${channelDetails.displayName} Twitch store!`, emailMessage, recipient);
+                    console.log(`Notifying ${recipient}.`);
+                    await sendEmail(`New items in the ${channelName} Twitch store!`, emailMessage, recipient);
                 }
+                console.log('Finishing...');
             }
+        }
+        if (!newItensFound) {
+            console.log('Nothing new was found.');
         }
     } catch (error) {
         console.error('Error during store update check:', error.message);
